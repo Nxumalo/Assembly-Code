310154fcf2c7635865bbcecd46f683199886f65c
Fraser <3538264@myuwc.ac.za>
2020-06-08 16:14:24 +0200

    Hello World Procedure

---
+++ helloworld_procedure.asm
@@ -0,0 +1,26 @@
+.model small
+.code
+main proc
+     mov ax,@data
+     mov ds,ax
+     lea dx,Hello
+     call Display_Dx
+     mov ah,4ch
+     mov al,00h
+     int 21h
+     
+Display_Dx Proc
+         Ret
+Display_Dx ENDP
+
+Main ENDP
+
+.Data
+
+Hello DB 'Hello!$'
+
+.Stack
+      DB 128 Dup (?)
+       
+      END Main 
+      
