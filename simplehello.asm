18e91d68dbeddb492a1f5cb8c8d4b3d5b956f924
Fraser <3538264@myuwc.ac.za>
2020-06-08 11:35:43 +0200

    Hello World

--- simplehello.asm
+++ simplehello.asm
@@ -3,7 +3,7 @@
 Hello Db 'Hello! Neo Nxumalo the Matrix has found You :--:---:--. $'
 .code
 .startup
-          lea Dx,Hello
+          lea Dx,Hello World
           mov AH,09h
           int 21h
           mov ax,4C00h
