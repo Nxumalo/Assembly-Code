70730bf1c5a05fcef4a965275ec1587c33bb12d7
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-08-21 15:09:45 +0200

    Hello Program

--- hello.asm
+++ hello.asm
@@ -1,19 +1,24 @@
-Segment Para Public 'Code'
-Assume CS:OurCode, DS:OurData, SS:OurStack
-         Mov ax, OurData
-         Mov DS,ax
-         Lea Dx,Hello
-         Mov AH,09h
-         Int 21h
-         Mov AH,09h
-         Mov AL,00h
-         Int 21
-Ends
-Segment Para Public 'Data'
-        Db 'Hello!$'
-Ends
-Segement Para Stack 'Stack'
-        Db 64 Dup(?)
-Ends
-End     Start
+TITLE hello example program ; title is not necessary 
+
+OurCode Segment Para Public 'Code'           ; declare code segment 
+         Assume CS:OurCode, DS:OurData, SS:OurStack
+Start:         Mov ax, OurData      ;copy address of data
+               Mov DS,ax            ;segment into ds 
+               Lea Dx,Hello         ;address of message
+               Mov AH,09h           ;DOS service "output text string"
+               Int 21h              ;DOS  service call
+               Mov AH,09h           ;DOS serice "terminate process"
+               Mov AL,00h           ;return code zero
+               Int 21h              ;DOS service call
+OurCode                  ENDS       ;end of code segment 
+
+OurData         Segment Para Public 'Data'   ;declare data segment 
+Hello        DB 'Hello!$'                    ;define string to display
+OurData                  ENDS                ;end of segment 
+
+OurStack Segement Para Stack 'Stack'         ;declare stack segment
+        DB 64 Dup(?)                         ;reserve 64 bytes
+OurStack         ENDS                        ;end of stack segment
+                  
+                  END     Start              ;end of program
 
