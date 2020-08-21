74590c12dadd3a33128e1e0d3d380af1e5f64272
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-08-21 14:22:52 +0200

    Flags

--- FLAGS.asm
+++ FLAGS.asm
@@ -2,46 +2,67 @@
 .code
 Begin:
 
-sub ax,ax
-add ax,1
-add ax,0FFFFh
-add ax,1
+;PART 1 The effects of Zero Flags demmonstration
 
-sub ax,ax
-sub ax,1
-add ax,2
+sub ax,ax ; Clear AX-Zero equals ZR
+add ax,1  ; Zero Flag becomes NZ
+add ax,0FFFFh ; Zero Flag becomes ZR
+add ax,1  ; Zero Flag becomes NZ again
 
-sub ax,ax
-mov ax,OFFFFh
-add ax,1
-add ax,0
-sub ax,1
+;Part 2 The effects to Sign Flag Demostrations 
 
-sub ax,ax
-mov ax,8
-add ax,8
-add ax,0
-sub ax,1
+sub ax,ax ; Sign flag must be PL
+sub ax,1 ; Sign flag becomes NG 
+add ax,2 ; Sign flag becomes PL
 
-sub ax,ax
-mov a1,128
-add al,128
+;Part 3 The effects to Carry Flag (CF) demonstration
 
-sub ax,ax
+sub ax,ax ; Clear AX- CF must be NC
+mov ax,OFFFFh ; Carry flag is not affected
+add ax,1 ; Carry flag becomes CY
+add ax,0 ; Carry flag becomes NC
+sub ax,1 ; Carry flag becomes CY
+
+;Part 4 The effects to Auxilliary Carry Flag
+
+sub ax,ax ; Auxiliary Carry gets NA
+mov ax,8 ; FLAG IS NOT AFFECTED
+add ax,8 ; Auxiliary carry gets AC
+add ax,0 ; Auxiliary Carry gets NA
+sub ax,1 ; Auxiliary Carry gets AC
+
+;Part 5 The effects to Overflow Flag demostration
+
+sub ax,ax ; Overfkiw flag must be NV
+; Addition
+mov a1,128 ; Overflow flag is not affected 
+add al,128 ; Overflow flag becomes OV
+
+; Subtraction
+
+sub ax,ax ; Overflow flag becomes NV
 add ax,8000h
-sub ax,7FFFh
+sub ax,7FFFh ; Overflow flag becomes OV
 
-sub ax,ax
-mov al,16
-mov bx,16
-mul bl
+; Multiplication
 
-sub ax,ax
-add ax,1
-add ax,10h
-add ax, 0100h
-mov ax,4C00h
-int 21h
+sub ax,ax ; Overflow flag must be NO
+mov al,16 ; The first factor into AL
+mov bx,16 ; The second factor into BL (BH = 0)
+mul bl ; Product = 256 in AX, Overfkiw fkag us OV,
+;     Carry flag  = CY
+
+; Part 6 The effects of Parity Flag demonstration
+sub ax,ax ; Parity flag must be PE
+add ax,1 ; Parity flag becomes PO
+add ax,10h ; parity flag becomes PE
+add ax, 0100h ; Parity flag unchanged
+
+; Part 7 Finish the Program
+
+mov ax,4C00h  ; AH - Dose servic number,
+              ; Al - Return Code
+int 21h       ; Dos service Call
 .data
 .stack
 end begin
