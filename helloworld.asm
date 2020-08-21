4fa57a8324ba9f8a2b5edb0be1eaf8f12520ad7c
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-08-21 15:15:33 +0200

    Hello World

--- helloworld.asm
+++ helloworld.asm
@@ -1,15 +1,17 @@
-.model small
-.code
-Start:  mov ax,@Data
-        mov Ds,ax
-        lea Dx,Hello
-        mov AH,09h
-        int 21h
-        mov Ah,4Ch
-        mov Al,00h
-        Int 21h
-.Data
-Hello Db 'Hello!$'
+TITLE Hello World       ;title is not necessary
+.model small            ;declare memory model
+.code                   ;declare code segment
+Start:  mov ax,@Data    ;copy address of data
+        mov Ds,ax       ;segment into DS
+        lea Dx,Hello    
+        mov AH,09h      ;define DOS function number 
+        int 21h         ;call DOS function to display 
+        mov Ah,4Ch      ;define DOS function to exit
+        mov Al,00h      ;with code sero
+        Int 21h         ;exit to DOS
+        
+.Data                   ;declare data segment 
+Hello Db 'Hello!$'      ;define string to be
 displayed
-.Stack
+.Stack                  ;declare stack segment
 End Start
