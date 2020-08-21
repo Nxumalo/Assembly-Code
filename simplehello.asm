71bc4f0366af04132491cd83f3289c4de0597f59
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-08-21 15:34:37 +0200

    simplified directive of hello world
     using com module

--- simplehello.asm
+++ simplehello.asm
@@ -1,11 +1,12 @@
+TITLE Hello A Simple Hello    ;title is not necessary
 .model tiny
 .data
-Hello Db 'Hello! Neo Nxumalo the Matrix has found You :--:---:--. $'
+Hello Db 'Hello! Neo Nxumalo the Matrix has found You :--:---:--. $' ;define string to display
 .code
 .startup
-          lea Dx,Hello World
-          mov AH,09h
-          int 21h
-          mov ax,4C00h
-          int 21
+          lea Dx,Hello World            ;DS:DX -  effectove address of string
+          mov AH,09h                    ;function 09h -  output text string
+          int 21h                       ;DOS service call
+          mov ax,4C00h                  ;function 4Ch - terminate process 
+          int 21h                       ;DOS service call
           End
