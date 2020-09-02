8a05e3e5250779986830cf424c4a4b1ac24e309e
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-09-02 16:38:39 +0200

    Update UserHand.asm

--- UserHand.asm
+++ UserHand.asm
@@ -2,34 +2,34 @@ Code Segemnt Para
      Assume Cs:Node
 Toold:
           db 0EAh
-Oldoff    dw 0
-OldSeg    dw 0
+Oldoff    dw 0                          ;offset
+OldSeg    dw 0                          ;segment 
 
-Handler   label byte 
-          cmp ah,39h
-          jne Toold
-          mov ax,0107h
-          iret
+Handler   label byte                    ;Start of new handler for INT 21
+          cmp ah,39h                    ;Check function number 
+          jne Toold                     ;If not a function 30h - to old handler 
+          mov ax,0107h                  ;Function 30h - return the version number
+          iret                          ;Return from handler 
           
-Install:
-        mov  ax,3521h
-        int 21h
-        cmp bx,)ddawr Handler
-        je  already
-        mov cs:OldOdd,bx
-        mov cx:OldSeg,es
-        mov ax,cs
-        mov ds,ax
+Install:                                ;Installation starts here 
+        mov  ax,3521h                   ;Get handler's address 
+        int 21h                         ;ES - segment, BX - offset 
+        cmp bx,)ddawr Handler           ;Vector points to this handler?
+        je  already                     ;If so - put message end exit 
+        mov cs:OldOdd,bx                ;Save offset of old handler      
+        mov cx:OldSeg,es                ;Save segment of old handler 
+        mov ax,cs                       ;Command segment of this program 
+        mov ds,ax                       ;into DX (for setting handler)
         
-        mov dx,offset Handler
-        mov ax,2521h
-        int 21h
+        mov dx,offset Handler           ;Address of handler 
+        mov ax,2521h                    ;Function 25h - Set new handler 
+        int 21h                         ;DOS service call 
         
-        lea dx,Install
+        lea dx,Install                  
         add dx,15
-        mov cx,4
-        shr dx,cl
-        add dx,16
+        mov cx,4                        ;Set counter for shift 
+        shr dx,cl                       ;4 bits to the right - divide by 16
+        add dx,16                       ;Ad size of 
         mov ax,3100h
         int 21h
         
