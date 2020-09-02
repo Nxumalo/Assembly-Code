b7ce305a0263ecf54416c25aa8b66adf3ad77b99
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-09-02 16:30:10 +0200

    A Sample of Direct Access to the Interrupt
    Vector Table

--- IntLocD.asm
+++ IntLocD.asm
@@ -1,3 +1,14 @@
+;      Program IntLocD
+;      _____________________________________________________
+;
+;      Muizenberg, 20 September 20
+;      _____________________________________________________
+;
+;      This is a sample of an assembler program that uses a simple 
+;      DOS service. It outputs the table of interrupts onto the 
+;      system output device and repots the corresponding handler is located. 
+;      The letter B means BIOS, the letter D - DOS, the letter N - not set (dummy handler)
+
 .model small
 IntSeg segment at 0
 IntVec dw 512 dup(?)
@@ -13,58 +24,55 @@ NumIntL db 0
       assume es:IntSeg
       mov ax,@Data 
       mov ds,dx
+      
       mov ax,IntSeg
       mov es,ax
       
-      mov cx,16
-
-Rows: 
-      push cx
-      mov di,0
-      mov cx,16
+      mov cx,16                    ;Line counter 
+Rows:                              ;Lines cycles starts here 
+      push cx                      ;Save outward counter       
+      mov di,0                     ;Counter within line 
+      mov cx,16                    ;Columns counter 
       mov al,NumInt
       mov NumIntL,al
-
 Intrs:
       mov bh,0
-      mov al,NumintL
+      mov al,NumintL               ;Load interrupt numbers 
       mov bl,al
       shl bx,1
       sh1 bx,1
       mov dx,IntVec[bx+2]
-      cmp dx,0A000h
-      ja  InBios
-      mov tbint[di+3],'D'
-      jmp doneind
-      
-InBios: mov tbint[di+3]
-DoneInd: 
-
-       cmp byte es:[bx],0CFh
-       jne PresHan
-       mov tbint[di+3]
+      cmp dx,0A000h                ;Compare to BIOS start address
+      ja  InBios                   ;If DX is greater - handler is in BIOS 
+      mov tbint[di+3],'D'          ;Set indicator 'DOS'
+      jmp doneind                  ;To the end of block 
+InBios: mov tbint[di+3]            ;Set indcator 'BIOS'
+DoneInd:                           ;This is the end of block 
        
+       cmp byte es:[bx],0CFh       ;First instruction IRET?
+       jne PresHan                 ;If not - handler presented 
+       mov tbint[di+3]             ;Set indicator 'Not set'
 PresHan: 
-        mov ah,0
-        mov dl,16
-        div dl
+        mov ah,0                   ;AL keeps the interrupt number 
+        mov dl,16                  ;Prepare to convert AL to symbols 
+        div dl                     ;AX/16
         
         mov bx,offset HexSym
         xlat
-        mov byte ptr tbint [di],al
+        mov byte ptr tbint [di],al ;symbol into output line 
         mov al,ah
         xlat
-        mov byte ptr tbint[di+1],al
+        mov byte ptr tbint[di+1],al ; symbol into output line 
         
-        add di,5
-        add NumIntL,16
-        loop Intrs
-        pop cx
-        mov ah,09
-        mov dx,offset tbint 
-        int 21h
-        inc NumInt
-        loop Rows
+        add di,5                   ;Next position in the line 
+        add NumIntL,16             ;Increase interrupt number 
+        loop Intrs                 ;Next step - next interrupt 
+        pop cx                     ;Restore cycle counter 
+        mov ah,09                  ;Function 09 - output string 
+        mov dx,offset tbint        ;Address of string DS:DX
+        int 21h                    ;Output one string 
+        inc NumInt                 ;
+        loop Rows                  ;Next step - next string  
         mov ax,4C00h
         int 21h
         end
