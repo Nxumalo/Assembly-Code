099d08ae6d5a8130f383446027e5e2e5fe22712e
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-09-02 12:28:47 +0200

    The Hello Program with Procedure

--- helloworld_procedure.asm
+++ helloworld_procedure.asm
@@ -1,26 +1,30 @@
-.model small
-.code
-main proc
-     mov ax,@data
-     mov ds,ax
-     lea dx,Hello
-     call Display_Dx
-     mov ah,4ch
-     mov al,00h
-     int 21h
+Title Hello Example Program        ;title is not necessary
+.model small                       ;declare memory model
+.code                              ;declare code segment 
+main proc                          ;declare main procedure 
+     mov ax,@data                  ;copy address of data
+     mov ds,ax                     ;segment into DS
+     lea dx,Hello                       
+     call Display_Dx               ;call procedure
+     mov ah,4ch                    ;define DOS function to exit 
+     mov al,00h                    ;with code zero
+     int 21h                       ;exit to DOS
      
-Display_Dx Proc
-         Ret
-Display_Dx ENDP
+     ;Procedure section
+     
+Display_Dx Proc                    ;deckare procedure Display_DX
+         Ret                       ;return to MAIN
+Display_Dx ENDP                    ;end of procedure
 
-Main ENDP
+     ;End of procedure section
+     
+Main ENDP                          ;end of code section
 
-.Data
+.Data                              ;declare data segment 
+Hello DB 'Hello! Neo :--:. $'      ;define string to display
 
-Hello DB 'Hello! Neo :--:. $'
-
-.Stack
-      DB 128 Dup (?)
+.Stack                             ;Declare stack segment 
+      DB 128 Dup (?)               ;reserve 128 byte for stack
        
-      END Main 
+      END Main                     ;end of program
       
