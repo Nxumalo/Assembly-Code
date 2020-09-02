de7293a71d7c025564c826f5e74dfe9ac96f20b1
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-09-02 15:30:12 +0200

    Output Symbols Using BIOS and DOS service

--- BIOServ.asm
+++ BIOServ.asm
@@ -1,15 +1,22 @@
+;This is an example of using two different levels
+;of output procedures - BIOS service and DOS service.
+
+;Note that BIOS procedure outputs the symbol
+;"BELL" as visible symbol while the DOS procedure 
+;takes the corresponding action - lets look and hear!
+
 .model small
 .code
-      mov ah,0Ah
-      mov al,07h
-      mov bh,0
-      mov bl,0
-      mov cx,1
-      int 10h
+      mov ah,0Ah             ;function 0Ah - output symbol
+      mov al,07h             ;AL - symbol to be output (BELL)
+      mov bh,0               ;Video page is supposed to be 0
+      mov bl,0               ;Used in graphic mode - hear not needed
+      mov cx,1               ;CX - number of symbols  
+      int 10h                ;BIOS video service call
       
-      mov ah,02
-      mov dl,al
-      int 21h
+      mov ah,02              ;Function 02h - output symbol
+      mov dl,al              ;DL - symbol to be output 
+      int 21h                ;DOS service call 
       
 .exit 
   end
