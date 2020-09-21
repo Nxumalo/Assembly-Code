506b9f68276fab253b2cc51886a54e442089aa39
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-09-21 13:46:31 +0200

    Pushing Keystrokes into Keyboard Buffer

--- pushkbd.asm
+++ pushkbd.asm
@@ -5,30 +5,30 @@
 .startup
 		mov 	 cx,10
 LP: 	
-		push	 cx
-		mov 	 ah,05
-		mov 	 ch,20h
-		mov  	 cl,'d'
-		int 	 16h
+		push	 cx			
+		mov 	 ah,05			;Function 05h - Push scan/ASCII code
+		mov 	 ch,20h			;Scan code for key "D"
+		mov  	 cl,'d'			;ASCII cpde for key "D"
+		int 	 16h			;BIOS keyboard service 
 		
-		mov 	 ah,05
-		mov      ch,13h
-		mov 	 cl,'r'
-		int 	 16h
+		mov 	 ah,05			;Function 05h - Push scan/ASCII code
+		mov      ch,17h			;Scan code for key "I"
+		mov 	 cl,'i'			;ASCII cpde fpr key "I"
+		int 	 16h			;BIOS keyboard service 
 		
-		mov 	 ah,05
-		mov  	 ch,13h
-		mov 	 cl,'r'
-		int 	 16h
-		
-		mov 	 ah,05
-		mov      ch,1Ch
-		mov 	 cl,13
-		int 	 16h
+		mov 	 ah,05			;Function 05h - Push scan/ASCII code
+		mov  	 ch,13h			;Scan code for key "R"
+		mov 	 cl,'r'			;ASCII code fpr key "R"
+		int 	 16h			;BIOS keyboard service
+			
+		mov 	 ah,05			;Function 05h - Push scan/ASCII code 
+		mov      ch,1Ch			;Scan code for ENTER key
+		mov 	 cl,13			;ASCII code for ENTER key
+		int 	 16h		`	;BIOS keyboard service 
 		pop      cx
 		loop 	 lp
 		
 Finish:	
-		mov 	 ax,4C00h
-		int 	 21h
+		mov 	 ax,4C00h		;Function 4Ch - terminate process
+		int 	 21h			;DOS service call
 		end
