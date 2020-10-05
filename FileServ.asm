1313319bc85999ecb54307bf2e9c856143676f0b
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-10-05 15:00:29 +0200

    The Set Of Clipper Functions
    For Getting the File Information

--- FileServ.asm
+++ FileServ.asm
@@ -1,5 +1,10 @@
 DTA db 21 dup (?)
-Attrib	db ?
+Attrib	db ?	;=========	; 01 - Read Only 
+				; 02 - Hidden 
+				; 04 - System 
+				; 08 - Volume Id 
+				; 10h - Directory 
+				; 20h - Archive 
 Time 	dw ?
 Date 	dw ?
 SizeF	dd 1951
@@ -9,28 +14,27 @@ NameF 	db 13 dup ('')
 Attrib 	db 2
 SrcFile db 76 dup (0)
 
-			WORKFUNC
+			WORKFUNCS
 			
 FindF	proc near
-AccPrm: 	
-		mov al,es:[si+bx]
-		cmp al,''
-		jnew NotBlank
-		mov al,0
-
-NotBlank: 
-			mov SrcFile[bx],al
-			inc bx
-			loop AccPrm
-			
-			lea dx,DTA
-			mov ah,1Ah
-			int 21h
-			
-			lea dx,SrcFile
-			mov cx,3Fh
-			mov ah,4Eh
-			int 21h
+AccPrm: 					;
+		mov al,es:[si+bx]		; Take one byte from file name 
+		cmp al,''			; Is it blank?
+		jnew NotBlank			; If not - proceed the next byte
+		mov al,0			; Else replace it with 0
+NotBlank: 					;
+			mov SrcFile[bx],al	; Copy the byte into work area 
+			inc bx			; Increase  the counter 
+			loop AccPrm		; Next byte 
+						;
+			lea dx,DTA		;Load address pf DTA omtp DS:DX 
+			mov ah,1Ah		;Function 1Ah - 
+			int 21h			;DOS service call 
+						;
+			lea dx,SrcFile		;Address of ASCIIZ file name into DS:DX
+			mov cx,3Fh		;Attribute 3Fh - any file 
+			mov ah,4Eh		;Function 4Eh - FindFirst 
+			int 21h			;Dos service call 
 			ret
 FindF 		endp
 
@@ -39,68 +43,52 @@ FindF 		endp
 			CLfunc long FileSize<char fname>
 			CLcode
 			
-			mov bx,0
-			mov cx,80
-			les si,fname
+			mov bx,0		;
+			mov cx,80		; Maximal length of file name 
+			les si,fname		; File name address into ES:SI
 			
 			call FindF
 			
-			jnx Success
-			mov word ptr SizeF,-1
-			mov word ptr SizeF[2],-1
+			jnx Success			;If CARRY flag isn't set - successful
+			mov word ptr SizeF,-1		;else set return value t -1 
+			mov word ptr SizeF[2],-1	;to signal that file not found 
 			
 Success:
-
-			mov ax,word ptr SizeF
-			mov dx,word ptr, SizeF[2]
+			mov ax,word ptr SizeF		;Place length of file into DX:AX
+			mov dx,word ptr, SizeF[2]	;to return as FORTRAN function 
 			mov word ptr FILELEN[0],ax
 			mov word ptr FILELEN[2],dx
+			CLret  FILELEN 
+			
+			CLfunc long FileAttr <char fname>
+			CLcode
+			
+			mov bx,0
+			mov cx,80			;Maximal length of file name 
+			les si,fname			;File name address into ES:SI 
+		
+			mov word ptr FATTR[0],0
+			mov word ptr FATTR[2],0
 			Call FindF
 			
-			jc RetFlen 
+			jc RetFlwn 			;If CARRY flag isn't set - successful 
 			mov ah,0
-			mov al,byte ptr Attrib
+			mov al,byte ptr Attrib		;Place length of file into DX:AX
 			mov word ptr FATTR[0],ax
 			
 RetFlen:	Clret FATTR
 			CLfunc long CurDisk<>
 			CLcode
 			push ds
-			mov ah,32h
-			mov dl,0
+			mov ah,32h			;function 32h - get drive parameters
+			mov dl,0			; 0 - current drive 
 			int 21h
 			mov al,ds:[bx]
-			pop dsmov word ptr NDisk[2],0
+			pop ds
+			mov word ptr NDisk[2],0
 			mov ah,0
-			mov word ptr NDisk[0],ax
-			
+			mov word ptr NDisk[0],ax			
 RetNDisk:
-			Clret NDisk
+			CLret NDisk
 			END
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
