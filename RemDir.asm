7a3032b73f70b429b8b920c7a1406da5be3f5a2c
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-10-05 12:11:16 +0200

    Removing the Directory

---
+++ RemDir.asm
@@ -0,0 +1,16 @@
+...........................................................
+DATA SEGMENT 
+PATH DB "D:\DIRECT1\NEWDIR",0       ;ASCIIZ string for path 
+        ......................
+DATA ENDS 
+..........................................................................
+CODE SEGMENT 
+            mov ax,DATA             ;base address
+            mov ds,ax               ;of data segment 
+            lea dx,PATH             ;DS:DX - address of the path string 
+            mov ah,3Ah              ;function 3Ah - delete subdirectory 
+            int 21h                 ;call MS-DOS service
+            jc error                ;error, if carry flag = 1 
+            ...................................
+ CODE ENDS 
+ ..........................................................................
