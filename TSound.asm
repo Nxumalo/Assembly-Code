640873a9f3006f721db03631bd105ec1e25ff0dc
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-10-07 15:25:06 +0200

    The Calling Program for the Subroutine Sound

--- TSound.asm
+++ TSound.asm
@@ -1,21 +1,21 @@
-.model small
-EXTRN sound : far
+.model small                                    
+EXTRN sound : far                               ;external sub-program SOUND will be used
 .stack
 .data
 freq dw 0,988,880,784,699,659,587,523
 .code
 .startup
-        mov cx,7
+        mov cx,7                                ;number of signals produced 
         
-prop:   mov bx,cx
-        shl bx,1
-        mov di,word ptr freq[bx]
-        mov bx,50
-        call sound 
-        loop prod
+prop:   mov bx,cx                               ;number of current signal 
+        shl bx,1                                ;multiply number by 2 to obtain offset 
+        mov di,word ptr freq[bx]                ;get frequency of current signal 
+        mov bx,50                               ;duration - 25 hundredths of second
+        call sound                              ;this produces sound 
+        loop prod                               ;next signal 
         
-        mov ax,4C00h
-        int 21h
+        mov ax,4C00h                            ;function 4C - terminate process 
+        int 21h                                 ;DOS service call 
         
   end      
         
