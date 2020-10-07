278603c871a23c050240e8e64b8a8d93901e77fd
Fraser Nxumalo <3538264@myuwc.ac.za>
2020-10-07 15:21:30 +0200

    Controlling the Speaker

--- Sound.asm
+++ Sound.asm
@@ -1,3 +1,24 @@
+;*********************************************************************************************************
+;
+; Program for outputting a sound of prescribed tone
+; 
+; Author: F.Nxumalo Unversity of the Western Cape, South Africa 2019 - 2020
+;___________
+; Call from Assembler program 
+;
+; Call Sound 
+;
+; Parameters passed through the registers:
+;
+; Frequency - DI register (from 21 to 65535 hertz)
+;
+; Duration - BX register (in hundredth of second)
+;
+; Register AX,CX,DX,DS,ES,SI are retained by the program
+;
+;
+;
+;**********************************************************************************************************
 PUBLIC SOUND
 CODE SEGEMENT
     ASSUME CS:CODE
@@ -8,45 +29,45 @@ SOUND PROC FAR
       push ds
       push es
       push si
-      
-      in al,61h
-      mov cl,al
-      or al,3
-      out 61h
-      mov al,0B6h
-      out 43h,al
-      mov dx,14h
-      mov ax,4F38h
-      div di
-      out 42h,al
-      mov al,ah
-      out 42h,al
-      
-      mov ax,91
-      mul bx
-      mov bx,500
-      div bx
-      mov bx,ax
-      mov ah,0
-      int 1Ah
-      add dx,bx
-      mov bx,dx
-      
-Cycle: int 1Ah
-       cmp dx,bx
-       jne Cycle
-       in al,61h
-       mov al,cl
-       and al,0FCh
-       out 61h,al
-       
-Exit:  pop si
-       pop es
-       pop ds
-       pop dx
-       pop cx
-       pop ax
-       RETF
+;_______________________________________________      
+      in al,61h                     ;Read current port mode B (8255)
+      mov cl,al                     ;Save current mode 
+      or al,3                       ;Switch on speaker and timer 
+      out 61h                       ;
+      mov al,0B6h                   ;set for channel 2 (8253)
+      out 43h,al                    ;command register 8253
+      mov dx,14h                    ;
+      mov ax,4F38h                  ;frequency divisor 
+      div di                        ;
+      out 42h,al                    ;lower byte of frequency 
+      mov al,ah                     ;
+      out 42h,al                    ;higer byte of frequency 
+; Generation of sound delay 
+      mov ax,91                     ;multiplier - AX register !
+      mul bx                        ;AX = BX*91 (result in BX:AX)
+      mov bx,500                    ;divisor, dividend in DX:AX
+      div bx                        ;result in AX, remainder in DX !
+      mov bx,ax                     ;save result 
+      mov ah,0                      ;read time 
+      int 1Ah                       ;
+      add dx,bx                     ;
+      mov bx,dx                     ;      
+Cycle: int 1Ah                      ;
+       cmp dx,bx                    ; Has time gone ?
+       jne Cycle                    ;
+       in al,61h                    ;Read mode of port B 
+       mov al,cl                    ;Previous mode 
+       and al,0FCh                  ;
+       out 61h,al                   ;Restore mode 
+;________________________________________
+; Restoring registers and exit 
+Exit:  pop si                       ;
+       pop es                       ;
+       pop ds                       ;
+       pop dx                       ;   
+       pop cx                       ;
+       pop ax                       ;
+       RETF                         ; exit from subtroutine 
         
 SOUND  ENDP
 CODE ENDS
