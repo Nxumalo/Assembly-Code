2239f7cc51be2d29fa2c54032f7559713d3ac21e
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-21 23:48:22 +0200

    Calculating Factorial Program

--- FACTR.asm
+++ FACTR.asm
@@ -1,13 +1,14 @@
 .nolistmacro
 
-;                                                     Program Factr
+;                                  Program Calculating Factorials
 
-;                Muizenberg Cape Town South Africa, 21 August 2020
+;                Muizenberg Cape Town South Africa, 20 May 2021
 
-; This is a sample of an assembler program which calculates the function called n-factorial 
+; This is a sample of an assembler 
+; program which calculates the function called n-factorial 
 
-.model SMALL      ; This defines memory model
-            IF1   ; Of first pass
+.model SMALL                    ; This defines memory model
+            IF1                 ; Of first pass
             include maclib.inc  ; open macro library 
             ENDIF               ; End of macro including block
 .code
@@ -19,24 +20,25 @@ Next:
       cmp d1,1Bh              ;Check if ESC key
       je ExCycle              ;Exit if pressed
       
-      mov ax,1                ; The initiall value of factorial 
-      cmp cx,1                ; Check the input value
+      mov ax,1                ;The initiall value of factorial 
+      cmp cx,1                ;Check the input value
       jl Prtres               ;Skip the calculating if the number is less than 1
       mov bx,0                ;Clear the work register
       
 Multi:
-      mov dx,0                ; Clear the high part of result
-      inc bx                  ; Increase the work regist
+      mov dx,0                ;Clear the high part of result
+      inc bx                  ;Increase the work regist
       mul bx                  ;The next result
-      loop Multi              ; Continue the cycle      
+      loop Multi              ;Continue the cycle      
   
 PrtRes:
       OutIntax                ;Put result on screen
       NewLine                 ;Move curse to the next line
       jmp Next                ;Proceed next number
 
-ExCycle:                      ; This label marks end of program                        
-      mov ax,4C00h            ; AH register contains 4Ch code that is a Dos Function number,AL register contains return code 00
-      int 21h                 ; Dos service "Terminate program"
-.stack                        ; This line defines the STACK segment 
+ExCycle:                      ;This label marks end of program                        
+      mov ax,4C00h            ;AH register contains 4Ch code that 
+                              ;is a Dos Function number,AL register contains return code 00
+      int 21h                 ;Dos service "Terminate program"
+.stack                        ;This line defines the STACK segment 
 end Begin
