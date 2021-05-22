91790befe767707c502f84bf13a49ba591a2b78b
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-22 18:53:26 +0200

    .EXE Program

--- helloworld.asm
+++ helloworld.asm
@@ -1,14 +1,14 @@
-TITLE Hello World       ;title is not necessary
+TITLE Hello World       ; https://www.silentcode.za
 .model small            ;declare memory model
 .code                   ;declare code segment
-Start:  mov ax,@Data    ;copy address of data
-        mov Ds,ax       ;segment into DS
+Start:  mov Ax,@Data    ;copy address of data
+        mov Ds,Ax       ;segment into DS
         lea Dx,Hello    
-        mov AH,09h      ;define DOS function number 
+        mov Ah,09h      ;define DOS function number 
         int 21h         ;call DOS function to display 
         mov Ah,4Ch      ;define DOS function to exit
         mov Al,00h      ;with code sero
-        Int 21h         ;exit to DOS
+        int 21h         ;exit to DOS
         
 .Data                   ;declare data segment 
 Hello Db 'Hello!$'      ;define string to be
