405a68dbffc089300e8a6b83aab8e222213d7181
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-05-26 21:26:56 +0200

    Video Hardware

---
+++ VideoHardware.asm
@@ -0,0 +1,21 @@
+;...........................................................................
+BlankStr db 80 dup ('',17h)     ; Blank string (white on blue -17h);
+;...........................................................................
+
+        mov ax,0B800h      ;0B800h - address of video buffer (color)
+        mov es,ax          ;ES points to video buffer
+        mov cx,25          ;repeat 25 times for whole screen
+RepOut:    mov di,0        ;ES:DI , effective address of video buffer start
+        lea si,BlnkStr     ;DS:SI effective address of text to output
+        Con10 dw,10  
+        push cx            ;save strings counter
+        mov cx,80          ;total length of string 
+rep     mov sb             ;send string to video memory
+        pop cx             ;restore stringd counter
+        add es,Con10       ;increase effective address by 160
+        loop RepOut
+        mov al,''          ;character to be output
+        mov bl,17h         ;attribute (white on blue)
+        int 10h            ;BIOS video service 
+        
+        end
