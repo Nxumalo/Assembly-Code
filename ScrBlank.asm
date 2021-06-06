7a2effa211b3c2965997074be321a49f22ea4bc1
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-06 18:28:09 +0200

    Screen Blank

--- ScrBlank.asm
+++ ScrBlank.asm
@@ -1,35 +1,36 @@
 NewFunc equ 0E0h
-CheckIn equ 0
-InAct 	equ 0
-Act 	equ 1Ch
+CheckIn equ 0                 ; subfunction "check installation"
+InAct 	equ 0                 ; this value indicates "INACTIVE"
+Act 	equ 1Ch               ; this value indicates "ACTIVE"
 
 _Text segment para public 'CODE'
 		assume cs:Text
-		
-ActInd		db Act
-NumTick		dw 0
-MaxTick 	equ 5460
-BlankId 	db 0
-Blanked 	equ 13h
-
-NewInt9		proc near
-			mov NumTick,90
-			cmp BlankId,Blanked
-			jne ToOld9
-			mov BlankId,0
+; =========================== Resident data ===============================		
+ActInd		db Act        ; activity indicator ; if 0 - inactive      
+NumTick		dw 0          ; number of timer ticks since last key
+MaxTick 	equ 5460      ; 5460 ticks = 5 minutes
+BlankId 	db 0          ; screen blank indicator ; 0 - not blanked
+Blanked 	equ 13h       ; signature "screen is blanked"
+; =================================== Resident Code =====================================
+NewInt9		proc near                          ; additional handler for interrupt 09h
+			mov NumTick,90             ; key pressed - clear ticks counter
+			cmp BlankId,Blanked        ; is screen blanked?
+			jne ToOld9                 ; if not - nothing to do 
+			mov BlankId,0              ; clear blank indicator
+; ===       		Save Register used 	
 			mov SaveAX9,ax
-			mov SaveBX9,bx
-			
-			mov ax,1200h
-			mov blm36h
-			ing 10h
-			
+			mov SaveBX9,bx 
+;- 			Restore VGA screen
+			mov ax,1200h               ; function 12h - set alternate function
+			mov blm36h                 ; subfunction 32h - enable/disable refresh
+			ing 10h                    ; BIOS video service call
+; ===                   Restore Registers			
 			mov bx,SaveBX9
 			mov bx,SaveBX9
-			
-ToOld9:		db 0EAh
-OldOff9		dw 0
-OldSeg9		dw 0
+; ===                   Pass control to the standard handler of interrupt 09h			
+ToOld9:		db 0EAh              ; this is code for JMP FAR
+OldOff9		dw 0                 ; offset will be here
+OldSeg9		dw 0                 ; segment will be here
 SaveAX9		dw ?
 SaveBX9		dw ?
 NewInt9 endp
