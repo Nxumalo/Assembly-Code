023b340e75558402e0801bb734c63f7a0f161996
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-12 15:47:25 +0200

    Screen Output

--- ScreenOut.asm
+++ ScreenOut.asm
@@ -13,3 +13,25 @@ Start:      mov ax,DATA        ; data segement
             mov ds,ax          ; basing
             ...........
 ;---------------------------------------------------------------------------------------------------------------------
+; The external sub-program PUTSTR is called here
+; The necessary parameters are passed through the common
+registers.
+           lea  si,TEXTO     ; DS:SI - address of beginning of text
+           mov  ah,07h       ; attribute
+           mov  cx,80        ; maximum length of string
+           xor  dx,dx        ; beginning of screen output
+           xor  bh,bh        ; video page number 0
+           call PUTSTR       ; text string ouput
+           xor  ah,ah        ; wait for a key to be pressed
+           int  16h          ; read ASCII character into AL
+            ..........
+;---------------------------------------------------------------------------------------------------------------------
+; Finish the work and exit to MS DOS.
+
+Exit:      mov  ax,4COOh     ; return Code = 0
+           int  21h          ; read ASCII character int AL
+            ..........
+CODE       ENDS
+           END  Start
+;---------------------------------------------------------------------------------------------------------------------
+        
